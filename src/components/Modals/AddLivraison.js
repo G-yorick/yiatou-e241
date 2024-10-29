b688412fbbe0dcb386549f398560d4234d6ae77c
Gnzikoune <gnzikoune@gmail.com>
2024-10-29 03:51:47 +0100

    modifs encore

--- src/components/Modals/AddLivraison.js
+++ src/components/Modals/AddLivraison.js
@@ -2,67 +2,65 @@ import { LuPenSquare } from "react-icons/lu";
 import ButtonCta from "../Buttons/ButtonCta";
 
 const AddLivraison = ({onCreateLivraison}) => {
-    const onCreate = () =>{
+    const handleCreate = () => {
         try {
-            if(onCreateLivraison){
+            if(onCreateLivraison) {
                 onCreateLivraison()
             }
         } catch (error) {
             console.log("onCreateLivraison n'est pas une fonction.");
         }
     }
-  return (
-    <div className="relative px-3">
-        {/* Input conteneur */}
-      <div>
-        {/* input */}
-        <div className="my-3 ">
-          <div className="flex justify-between items-center">
-            <div className="flex gap-2 items-center">
-              <input type="radio" name="adresses" id="adresse-1" />
-              <label htmlFor="adresse-1" className="label font-bold">
-                Adresse 1
-              </label>
+
+    return (
+        <div className="relative px-3 -translate-y-4">
+            <div>
+                <div className="mb-2">
+                    <div className="flex justify-between items-center mb-1">
+                        <div className="flex gap-2 items-center">
+                            <input type="radio" name="adresses" id="adresse-1" />
+                            <label htmlFor="adresse-1" className="label font-bold">
+                                Adresse 1
+                            </label>
+                        </div>
+                        <button>
+                            <LuPenSquare />
+                        </button>
+                    </div>
+                    <p className="font-medium text-gray-700">
+                        Livraison a Mr. Dave Anguilet Walker, a l'adresse Beausejour en face de chez Mamboundou.
+                    </p>
+                </div>
+
+                <div className="mb-2">
+                    <div className="flex justify-between items-center mb-1">
+                        <div className="flex gap-2 items-center">
+                            <input type="radio" name="adresses" id="adresse-2" />
+                            <label htmlFor="adresse-2" className="label font-bold">
+                                Adresse 2
+                            </label>
+                        </div>
+                        <button>
+                            <LuPenSquare />
+                        </button>
+                    </div>
+                    <p className="font-medium text-gray-700">
+                        Livraison a Mr. Dave Anguilet Walker, a l'adresse Beausejour en face de chez Mamboundou.
+                    </p>
+                </div>
             </div>
-            <button>
-              <LuPenSquare />
+
+            <button 
+                onClick={handleCreate}
+                className="border-2 w-full py-1.5 rounded-full mb-2 mt-3 text-gray-600 font-medium"
+            >
+                Ajouter une adresse de livraison
             </button>
-          </div>
-          <p className="font-medium text-gray-700">
-            Livraison a Mr. Dave Anguilet Walker, a l'adresse Beausejour en face
-            de chez Mamboundou.
-          </p>
+            <ButtonCta>
+                Continuer
+            </ButtonCta>
         </div>
-        {/* end input */}
-        {/* input */}
-        <div className="my-3">
-          <div className="flex justify-between items-center">
-            <div className="flex gap-2 items-center">
-              <input type="radio" name="adresses" id="adresse-2" />
-              <label htmlFor="adresse-2" className="label font-bold">
-                Adresse 2
-              </label>
-            </div>
-            <button>
-              <LuPenSquare />
-            </button>
-          </div>
-          <p className="font-medium text-gray-700">
-            Livraison a Mr. Dave Anguilet Walker, a l'adresse Beausejour en face
-            de chez Mamboundou.
-          </p>
-        </div>
-        {/* end input */}
-      </div>
-      {/* fin input conteneur */}
-      <button onClick={onCreate} className="border-2 w-full py-2 rounded-full mb-3 mt-4 text-gray-600 font-medium">
-        Ajouter une adresse de livraison
-      </button>
-      <ButtonCta>
-        Continuer
-      </ButtonCta>
-    </div>
-  );
+    );
 };
 
 export default AddLivraison;
--- src/components/Modals/CreateLivraison.js
+++ src/components/Modals/CreateLivraison.js
@@ -3,7 +3,7 @@ import ButtonCta from "../Buttons/ButtonCta";
 const CreateLivraison = () => {
     return (
         <div className="w-full">
-            <textarea className="resize-none border-2 w-full min-h-[140px] p-3 rounded-lg mt-5 mb-1" placeholder="Nouvelle Adresse de livraison"></textarea>
+            <textarea className="resize-none border-2 w-full min-h-[140px] p-3 rounded-lg mt-10 mb-1" placeholder="Nouvelle Adresse de livraison"></textarea>
             <p className="text-[13px] font-medium text-gray-600 mb-4">La commande en cours sera livré a cette adresse.</p>
             <ButtonCta type="button">Ajouter</ButtonCta>
         </div>
