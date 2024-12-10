23ce4038fcfe245650532cc452deabfc416ab80b
YannSteeve <mouendouyann08.gmail.com>
2024-12-10 10:32:49 +0100

    premiercommit

---
+++ src/layout/BottomBarLayout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BottomBarLayout = () => {
+    return (
+        <div className="bottom-bar-layout">
+            {/* Bottom bar content goes here */}
+        </div>
+    );
+};
+
+export default BottomBarLayout;
